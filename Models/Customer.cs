ae533268b8c38579190bd1c95923cb3ad0a04b28
SwatiThorali <swatithorali555@gmail.com>
2022-02-15 16:49:32 -0800

    Add project files.

---
+++ Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerManagementSystem.Models
+{
+    public class Customer
+    {
+        [Key]
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Country { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ Models/EFDBhandle.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerManagementSystem.Models
+{
+    public class EFDBhandle:DbContext
+    {
+        public string conn = "Server=DESKTOP-Q5TBMF9;Database= CustomerMgmt_DB; Trusted_Connection=True;";
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsB)
+        {
+            optionsB.UseSqlServer(conn);
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
